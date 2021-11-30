aee465ecd636aea10c51a05a83234efea1a9a424
bibiyanki2 <93124252+bibiyanki2@users.noreply.github.com>
2021-11-30 17:59:53 +0100

    d

--- webcam.js
+++ webcam.js
@@ -63,7 +63,8 @@ if (navigator.mediaDevices.getUserMedia) {
                                 cardResults.cards.forEach(function(card){
                                     console.log(card);
                                     $("#possibleCards span:last").after('<span><p>' + card.name + '</p></span>'
-                                   
+                                    $("#possibleCards2 span:last").after('<span><a href="http://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=' 
+                                    + card.multiverseid + '" target="_blank"><img src="' + card.imageUrl +'"/></a></span>'
                                         );
                                     if(card.rulings.length !=0){
                                         card.rulings.forEach(function(rule){
