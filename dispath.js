8435d680246e5ee16a3cade5b535a23d9e9bc0fe
bibiyanki2 <93124252+bibiyanki2@users.noreply.github.com>
2021-12-01 20:20:46 +0100

    d

--- dispath.js
+++ dispath.js
@@ -1,7 +1,5 @@
 $(document).ready(function(){
    
    $('#webcamcard').load("html/webcamcard.html");
-   $('#printcard').load("html/printcard.html");
-   $('#analysecard').load("html/analysecard.html");
-   $('#buildcard').load("html/buildcard.html");
+   
 });
