1b3e95694189fd2e7c550336c5c2a119cfed2313
bibiyanki2 <93124252+bibiyanki2@users.noreply.github.com>
2021-12-04 10:45:02 +0100

    d

--- dispath.js
+++ dispath.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
    
    $('.headerindex').load("https://mtg-proxy.netlify.app/header.html");
-   $('.buildcard').load("html/buildcard.html");
-   $('.printcard').load("html/printcard.html");
+ 
 });
