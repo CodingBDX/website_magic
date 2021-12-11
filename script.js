28f5869ce12c115c7088cb6c5a45187b5ce529f8
bibiyanki2 <93124252+bibiyanki2@users.noreply.github.com>
2021-12-11 18:20:22 +0100

    g

--- script.js
+++ script.js
@@ -167,7 +167,7 @@ function appendCards(sources, quantity, isCustom, configuration) {
         const button = document.createElement("button");
         button.textContent = source.set;
         button.setAttribute("type", "button");
-        button.classList.add("uppercase", "text-base", "btn", "btn-danger", "absolute");
+        button.classList.add("uppercase", "text-base", "btn", "btn-danger", "absolute", "bottommagic");
         button.onclick = switchPrint;
         div.appendChild(button);
       }
