f13a0b380eca7bf7a732c8087dcfb30cce2a9055
bibiyanki2 <93124252+bibiyanki2@users.noreply.github.com>
2021-12-12 06:40:02 +0100

    g

--- myscript.js
+++ myscript.js
@@ -42,7 +42,7 @@ adaptiveHeight: true,
 
   var myFullpage = new fullpage('#fullpage', {
       anchors: ['Presentation', 'gallery_art_magic_the_gathering', 'take_picture_magic_the_gathering', 'Print_your_card_magic', 'create_proxy_card_magic'],
-      sectionsColor: ['#dd3d31', '#fff', '#fff', '#dd3d31'],
+      sectionsColor: ['#CA3C66', '#DB6A8F', '#fff', '#E8AABE'],
       scrollBar: true,
       responsiveWidth: 900,
      
