2ef49a01fae7eb8e16727fc9be8bc6575038a559
bibiyanki2 <93124252+bibiyanki2@users.noreply.github.com>
2021-12-12 12:08:24 +0100

    f

--- myscript.js
+++ myscript.js
@@ -42,7 +42,7 @@ adaptiveHeight: true,
 
   var myFullpage = new fullpage('#fullpage', {
       anchors: ['Presentation', 'gallery_art_magic_the_gathering', 'take_picture_magic_the_gathering', 'Print_your_card_magic', 'create_proxy_card_magic'],
-      sectionsColor: ['#374158', '#222D41', '#fff', '#11151D'],
+      sectionsColor: ['#585858', '#7E7E7E', '#fff', '#7E7E7E'],
       scrollBar: true,
       responsiveWidth: 900,
      
