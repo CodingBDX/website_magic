d85ae07cf5bb9441681ebc02601dbc3008162272
bibiyanki2 <93124252+bibiyanki2@users.noreply.github.com>
2021-12-13 18:43:50 +0100

    g

--- tokens.js
+++ tokens.js
@@ -15,7 +15,7 @@ function isValidLine(line)
 
 function clearLine(line)
 {
-    line = linne.replace(/[0-9]/gm, "")
+    line = linne.replace(/[0-9]/gm, "");
 	line = line.replace(/^\d+x/gm, ""); // Removes "2x"
 	line = line.replace(/\(.*\)/gm, "");  // Removes "(SET")"
 	line = line.replace(/\*\w+\*/gm, "");  // Removes "*F*"
@@ -199,7 +199,7 @@ function generateTokens()
 {
 	console.log("Generate called");
 	
-	var input = $(".inputtotkens").val();
+	var input = $("#input").val();
 	var output = [];
 	var lines = input.match(/[^\r\n]+/g);
 	var validLines = [];
