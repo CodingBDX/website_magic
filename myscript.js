df663e4f84061bf1a3e919a1a6106ba0ade65275
bibiyanki2 <93124252+bibiyanki2@users.noreply.github.com>
2021-12-14 11:16:04 +0100

    h

--- myscript.js
+++ myscript.js
@@ -49,8 +49,8 @@ adaptiveHeight: true,
   });
 
   var myFullpage = new fullpage('#fullpage', {
-      anchors: ['Presentation', 'gallery_art_magic_the_gathering', 'take_picture_magic_the_gathering', 'Print_your_card_magic', 'create_proxy_card_magic'],
-      sectionsColor: ['#051C24', '#051C24', '#fff', '#051C24'],
+      anchors: ['Presentation', 'gallery_art_magic_the_gathering', 'Print_your_card_magic', 'create_proxy_card_magic'],
+      sectionsColor: ['#051C24', '#051C24', '#051C24', '#051C24'],
       scrollBar: true,
       responsiveWidth: 900,
      
